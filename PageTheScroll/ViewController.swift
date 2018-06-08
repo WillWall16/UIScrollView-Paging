8a066debf60ddc94cd332e13671182861b0f482e
William Friedrich <willwall916@gmail.com>
2018-06-08 14:15:04 -0700

    Made UIImageView creator

--- PageTheScroll/ViewController.swift
+++ PageTheScroll/ViewController.swift
@@ -7,18 +7,29 @@
 //
 
 import UIKit
+var images = [UIImageView]()
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        for x in 0...2 {
+            let image = UIImage(named: "icon\(x).png")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            
+            newX = view.frame.midX + view.frame.size.width * CGFloat(x)
+            
+            scrollView.addSubview(imageView)
+            
+            imageView.frame = CGRect(x: 0, y: view.frame.size.height / 2, width: 150, height: 150)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
